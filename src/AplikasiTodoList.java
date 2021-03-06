8438d18a7fb50468db914211f9eba676b68fdd34
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-03-06 17:49:32 +0700

    menampilkan todolist

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -14,7 +14,17 @@ public class AplikasiTodoList {
      */
 
     public static void showTodoList(){
+        //tambahkan ke posisi yang data array nya NUll
+        for (int i = 0; i < data.length; i++) {
+            var todo = data[i];
+            var no = i +1;
 
+            //check data
+            if(todo != null){
+                System.out.println(no + ". " + todo);
+            }
+
+        }
     }
 
     /**
